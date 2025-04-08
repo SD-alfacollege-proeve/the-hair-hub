616806bde0de197611974c4589c8259ae3edc4c8
FrankZeeman <zeemanfrank@gmail.com>
2025-04-08 14:09:56 +0200

    Figured out how refs worked

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -2,6 +2,7 @@
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
+import { ref } from 'vue';
 import PlaceholderPattern from '../components/PlaceholderPattern.vue';
 
 const breadcrumbs: BreadcrumbItem[] = [
@@ -10,6 +11,9 @@ const breadcrumbs: BreadcrumbItem[] = [
         href: '/dashboard',
     },
 ];
+
+const isAdmin = ref(true);
+const isEmployee = ref(false);
 </script>
 
 <template>
@@ -19,7 +23,13 @@ const breadcrumbs: BreadcrumbItem[] = [
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <div class="border-sidebar-border/70 relative aspect-video overflow-hidden rounded-xl border">
-                    <PlaceholderPattern />
+                    <div>
+                        <div class="relative z-100">
+                            <div v-if="isEmployee">Welkom, werknemer!</div>
+
+                            <div v-else-if="isAdmin">Welkom, admin!</div>
+                        </div>
+                    </div>
                 </div>
                 <div class="border-sidebar-border/70 relative aspect-video overflow-hidden rounded-xl border">
                     <PlaceholderPattern />
